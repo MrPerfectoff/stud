d2c7db084f07265ca66ce16e7fa8f00c7d98e216
MrPerfectoff <bounce90@mail.ru>
2024-08-26 20:00:18 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,14 +1,12 @@
-def single_root_words(root_word = str, *other_words):
+def single_root_words(root_word, *other_words):
     same_words = []
-    root_word_low = root_word.lower()
     for i in other_words:
-        i = i.lower()
-        if root_word_low in i or i in root_word_low:
+        if root_word.lower() in i.lower() or i.lower() in root_word.lower():
             same_words.append(i)
     return same_words
 
 
 result = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 print(result)
-result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable',)
 print(result2)
