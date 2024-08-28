9f69d68edc81b18f9c0a5d0c3b1cf058fc8cff50
MrPerfectoff <bounce90@mail.ru>
2024-08-28 21:11:41 +0300

    Add files via upload

---
+++ Module_4_1.py
@@ -0,0 +1,10 @@
+from true_math import divide as one
+from fake_math import divide as two
+res1 = two(69, 3)
+print(res1)
+res2 = two(3, 0)
+print(res2)
+res3 = one(49, 7)
+print(res3)
+res4 = one(15, 0)
+print(res4)
