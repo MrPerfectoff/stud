a779a7add58535264d957f392f15fa930c995c47
MrPerfectoff <bounce90@mail.ru>
2024-12-09 12:42:58 +0300

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,24 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = "7938815284:AAHp1mJaKpvE7a0sNXPs784F26izYZDcUbM"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+
+@dp.message_handler(text = ["Urban", 'ff'])
+async def urban_message(message):
+    print("Urban Message")
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    print('Привет, я бот помогающий твоему здоровью')
+
+@dp.message_handler()
+async def all_message(message):
+   print("Введите команду /start, что бы начать общение")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
